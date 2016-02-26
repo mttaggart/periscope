7b655238e3d25cb8dac8a4d5dafcb600cdac7921
mttaggart <michael.t.taggart@gmail.com>
2016-02-25 20:22:07 -0800

    Add home route

--- periscope/app/controllers/units_controller.rb
+++ periscope/app/controllers/units_controller.rb
@@ -61,6 +61,11 @@ class UnitsController < ApplicationController
     end
   end
 
+  # HOME path
+  def home
+    @units = Unit.recent
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_unit
